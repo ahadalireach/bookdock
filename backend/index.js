db3e6394ced427c3f6228dd1156598642acd095f
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-08-30 16:58:12 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./db/db.js";
 import bookRouter from "./routes/bookRoutes.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
@@ -11,9 +12,18 @@ const PORT = process.env.PORT || 4000;
 connectDB();
 const app = express();
 
-app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'https://bookdock-web.vercel.app' 
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
